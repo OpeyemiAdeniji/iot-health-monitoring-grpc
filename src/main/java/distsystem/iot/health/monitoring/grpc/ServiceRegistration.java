5b2ed393b23f70ef6f238be2d569c4cdcfe279ff
Opeyemi Adeniji <adenijiopeyemi68@gmail.com>
2026-03-25 17:49:11 +0000

    add JmDNS service registration for service discovery

---
+++ src/main/java/distsystem/iot/health/monitoring/grpc/ServiceRegistration.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package distsystem.iot.health.monitoring.grpc;
+
+import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceInfo;
+
+
+/**
+ *
+ * @author opeyemiadeniji
+ */
+
+
+/**
+ * Singleton that registers gRPC services via mDNS (JmDNS).
+ * Binds to the first real LAN IPv4 address so discovery works on
+ * both Windows and macOS (avoids the macOS loopback multicast issue).
+ */
+public class ServiceRegistration {
+
+    private static JmDNS jmdns;
+    private static ServiceRegistration theRegister;
+
+    private ServiceRegistration() throws IOException {
+        InetAddress address = getLocalNonLoopbackAddress();
+        jmdns = JmDNS.create(address);
+        System.out.println("Registering on address: " + address);
+    }
+
+    public static ServiceRegistration getInstance() throws IOException {
+        if (theRegister == null) {
+            theRegister = new ServiceRegistration();
+        }
+        return theRegister;
+    }
+
+    public void registerService(String type, String name, int port, String text) throws IOException {
+        ServiceInfo serviceInfo = ServiceInfo.create(type, name, port, text);
+        jmdns.registerService(serviceInfo);
+        System.out.println("Registered Service: " + serviceInfo);
+    }
+
+    private InetAddress getLocalNonLoopbackAddress() throws IOException {
+        try {
+            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+            while (interfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = interfaces.nextElement();
+                if (!networkInterface.isUp() || networkInterface.isLoopback() || networkInterface.isVirtual()) {
+                    continue;
+                }
+                Enumeration<InetAddress> addresses = networkInterface.getInetAddresses();
+                while (addresses.hasMoreElements()) {
+                    InetAddress address = addresses.nextElement();
+                    if (address instanceof Inet4Address && !address.isLoopbackAddress()) {
+                        return address;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new IOException("Could not find non-loopback IPv4 address", e);
+        }
+        throw new IOException("No non-loopback IPv4 address found.");
+    }
+}
